0404359910b93cd9cf564ed5b2c8d6fe5eb6d337
Antti Halla <antti.halla@gmail.com>
2017-03-05 17:37:19 +0200

    Refactoring, do reduce in one pass

--- lib/mazing/traverse/bfs.ex
+++ lib/mazing/traverse/bfs.ex
@@ -7,46 +7,35 @@ defmodule Mazing.Traverse.Bfs do
 
     distances = %{}
       |> Map.put(s, 0)
-    visited = []
+    visited = MapSet.new
     prevs = %{}
       |> Map.put(s, -1)
     traverse_inner(g, q, distances, prevs, visited)
 
   end
 
-  def traverse_inner(g, q, distances, prevs, visited) do
-    if :queue.is_empty(q) do
+  def traverse_inner(g, queue, distances, prevs, visited) do
+    if :queue.is_empty(queue) do
       { distances, prevs }
     else
-      {{:value, {v, d}}, q} = :queue.out(q)
+      {{:value, {v, d}}, queue} = :queue.out(queue)
       neighbors = Digraph.adj(g, v)
 
-      visited = visited ++ [v]
-      q = Enum.reduce(neighbors, q, fn w, acc ->
-        if Enum.member?(visited, w) do
-          acc
-        else
-          :queue.in {w, d + 1}, acc
-        end
-      end)
+      visited = MapSet.put(visited, v)
+
       # distance gets overridden. how do I test for this?
-      distances = Enum.reduce(neighbors, distances, fn w, acc ->
+      {queue, distances, prevs} = Enum.reduce(neighbors, {queue, distances, prevs}, fn w, {acc_queue, acc_distances, acc_prevs} = acc ->
         if Enum.member?(visited, w) do
           acc
         else
-          Map.put acc, w, d
-        end
-      end)
-      prevs = Enum.reduce(neighbors, prevs, fn w, acc ->
-        if Enum.member?(visited, w) do
-          acc
-        else
-          Map.put acc, w, v
+          distances_ = Map.put acc_distances, w, d
+          prevs_ = Map.put acc_prevs, w, v
+          queue_ = :queue.in {w, d + 1}, acc_queue
+          {queue_, distances_, prevs_}
         end
       end)
 
-
-      traverse_inner(g, q, distances, prevs, visited)
+      traverse_inner(g, queue, distances, prevs, visited)
     end
 
   end
