6342267cf52e68a73c04a266473c810705b5ee40
Antti Halla <antti.halla@gmail.com>
2017-03-07 20:30:54 +0200

    Remove priv/static from repo. Add maze test

--- apps/mazing/test/maze_test.exs
+++ apps/mazing/test/maze_test.exs
@@ -1,7 +1,13 @@
 defmodule Mazing.MazeTest do
   use ExUnit.Case
 
+  alias Mazing.Maze
+  require Logger
 
+  test "generate_maze/2 generates something like a maze w empty trails" do
+    maze = Maze.generate_maze(:maze_server, %{generator: :sidewinder, size: 5})
+    assert %{graph: _graph, objects: _objects, trails: %{}} = maze
+  end
 
 
 end
